8537f88c271ab300cd05be37e2f3037514601605
Tiimor <tiimor@qq.com>
2023-03-04 20:35:03 +0800

    显示多个预览图

--- app/src/main/java/com/excellence/compose/wanandroid/MainActivity.kt
+++ app/src/main/java/com/excellence/compose/wanandroid/MainActivity.kt
@@ -3,10 +3,13 @@ package com.excellence.compose.wanandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import com.excellence.compose.wanandroid.ui.theme.ComposeWanAndroidTheme
 
@@ -16,7 +19,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeWanAndroidTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                ) {
                     Greeting("Android")
                 }
             }
@@ -26,13 +34,43 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column(verticalArrangement = Arrangement.Center) {
+        Text(text = "Hello $name!")
+        Text(text = "Hello World")
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    name = "default"
+)
 @Composable
 fun DefaultPreview() {
     ComposeWanAndroidTheme {
         Greeting("Android")
+    }
+}
+
+@Preview(
+    showBackground = true,
+    name = "Nexus",
+    device = Devices.NEXUS_10
+)
+@Composable
+fun DefaultPreviewNexus() {
+    ComposeWanAndroidTheme {
+        Greeting(name = "Nexus")
+    }
+}
+
+@Preview(
+    showBackground = false,
+    name = "TV",
+    device = Devices.PIXEL_XL
+)
+@Composable
+fun DefaultPreViewTV() {
+    ComposeWanAndroidTheme {
+        Greeting(name = "Pixel")
     }
 }
